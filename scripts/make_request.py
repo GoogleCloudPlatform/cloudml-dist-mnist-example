d6348aa9899fb33678910cb3d9a375eb43074975
Etsuji Nakai <enakai0@gmail.com>
2017-04-15 17:34:43 +0900

    Fix: input -> inputs.

--- scripts/make_request.py
+++ scripts/make_request.py
@@ -20,5 +20,5 @@ from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
 with open("request.json", "w") as file:
     for i in range(10):
-        data = {"input": mnist.test.images[i].tolist()}
+        data = {"inputs": mnist.test.images[i].tolist()}
         file.write(json.dumps(data) + '\n')
