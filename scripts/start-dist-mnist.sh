44ef5bcc8fda30ddd28c8f34d6ee4c0ef827d0e1
Etsuji Nakai <enakai0@gmail.com>
2018-03-05 11:09:10 +0900

    Fix: PYTHONPATH, TF version issue

--- scripts/start-dist-mnist.sh
+++ scripts/start-dist-mnist.sh
@@ -15,8 +15,8 @@
 # limitations under the License.
 
 ver=$(sudo pip list | grep -E '^tensorflow \(' | grep -oE '[0-9]+\.[0-9]+')
-if [[ $ver == '1.3' ]]; then
-  echo "TF1.3 currently has a compatibility issue with the prediction API."
+if [[ $ver != '1.2' ]]; then
+  echo "TF${ver} currently has a compatibility issue with the prediction API."
   echo "Downgrading to TF1.2.1 as a tentative workaround..."
   sudo pip install tensorflow==1.2.1
 fi
@@ -29,5 +29,6 @@ ROLE=$(hostname | awk -F'-' '{ print $1 }')
 INDEX=$(hostname | awk -F'-' '{ print $2 }')
 
 export TF_CONFIG=$(sed "s/__INDEX__/$INDEX/;s/__ROLE__/$ROLE/" tf_config.json)
+export PYTHONPATH="$PWD":"${PYTHONPATH}"
 
 python trainer/task.py --output_dir=$OUTDIR --data_dir=$DATADIR --train_steps=10000
